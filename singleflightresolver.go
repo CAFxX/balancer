c62978468ac040e3e00a0a392ceb21dff2043da6
Carlo Alberto Ferraris <cafxx@strayorange.com>
2022-07-17 00:49:02 +0000

    clone slice from singleflight

--- singleflightresolver.go
+++ singleflightresolver.go
@@ -23,9 +23,12 @@ type singleflightKey struct {
 var _ Resolver = &SingleflightResolver{}
 
 func (r *SingleflightResolver) LookupNetIP(ctx context.Context, af, host string) ([]netip.Addr, error) {
-	addr, _, err := r.sf.Do(ctx, singleflightKey{af, host}, func(ctx context.Context) ([]netip.Addr, error) {
+	addr, shared, err := r.sf.Do(ctx, singleflightKey{af, host}, func(ctx context.Context) ([]netip.Addr, error) {
 		defer r.sf.Forget(singleflightKey{af, host})
 		return r.Resolver.LookupNetIP(ctx, af, host)
 	})
+	if shared {
+		addr = clone(addr)
+	}
 	return addr, err
 }
